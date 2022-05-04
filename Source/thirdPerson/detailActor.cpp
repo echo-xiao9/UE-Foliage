d1a9b76e77db149d2874fed4f8bb57bf70e6b83e
echo-xiao9 <819601193@qq.com>
2022-05-04 19:05:19 +0800

    add left

---
+++ Source/thirdPerson/detailActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "detailActor.h"
+
+// Sets default values
+AdetailActor::AdetailActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AdetailActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AdetailActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/thirdPerson/thirdPerson.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "thirdPerson.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, thirdPerson, "thirdPerson" );
