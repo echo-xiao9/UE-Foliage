2d1a7d01062c379db7f6bb5c40c9b5f90e5bb217
Wang Tingyu <1291859874@qq.com>
2022-05-24 20:55:06 +0800

    reorganize plant layer enum class

---
+++ Plugins/FoliageGenerator/Source/tets/Public/FoliageGeneratorUtils.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class TETS_API FoliageGeneratorUtils
+{
+public:
+	enum class PlantLayer { Tree = 1, Bush = 2, Grass = 3, Flower = 4, Stone = 5 };
+
+	FoliageGeneratorUtils();
+	~FoliageGeneratorUtils();
+};
--- Plugins/FoliageGenerator/Source/tets/Public/PlantRecommend.h
+++ Plugins/FoliageGenerator/Source/tets/Public/PlantRecommend.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "DBApi.h"
+#include "FoliageGeneratorUtils.h"
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
 #include "PlantRecommend.generated.h"
@@ -16,11 +17,7 @@ class TETS_API UPlantRecommend : public UObject
 	GENERATED_BODY()
 
 public:
-	enum class PlantLayer { MainTree = 0x1, SubTree = 0x2, Bush = 0x4, Grass = 0x8 };
-
-	static TArray<FString> GetRecommendSubTrees(FString mainTreeName);
-	static TArray<FString> GetRecommendBushes(FString mainTreeName);
-	static TArray<FString> GetRecommendGrass(FString mainTreeName);
+	static TArray<FString> GetRecommendPlants(FString mainTreeName, FoliageGeneratorUtils::PlantLayer targetLayer);
 
 protected:
 	static int GetPlantTags(FString mainTreeName, TArray<DBApi::stringKV>& stringTags, TArray<DBApi::numberKV>& numberTags);
