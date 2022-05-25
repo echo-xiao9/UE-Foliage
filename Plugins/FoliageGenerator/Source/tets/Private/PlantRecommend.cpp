caddef2f20b8ceb4e48339d066cbc3e35e4fa924
Wang Tingyu <1291859874@qq.com>
2022-05-25 16:13:30 +0800

    add recommend about ecosystem & color

--- Plugins/FoliageGenerator/Source/tets/Private/PlantRecommend.cpp
+++ Plugins/FoliageGenerator/Source/tets/Private/PlantRecommend.cpp
@@ -13,11 +13,85 @@ TArray<FString> UPlantRecommend::GetRecommendPlants(FString mainTreeName, Foliag
 		// error handle
 	}
 
-	// special rules
-	//DBApi::numberKV* layerKV = numberTags.FindByPredicate([](DBApi::numberKV kv) { return kv.key == "Layer"; });
-	//layerKV->value = (float)PlantLayer::SubTree;
+	// phase ecosystem
+	TArray<FString> ecosytems;
+	DBApi::stringKV* ecosysKV = stringTags.FindByPredicate([](DBApi::stringKV kv) { return kv.key == "Ecosystem"; });
+	if (!ecosysKV) {
+		// ecosystem tag missing
+	}
+	FString ecosysPop, ecosysVal = ecosysKV->value;
+	while (ecosysVal.Split("/", &ecosysPop, &ecosysVal)) {
+		ecosytems.Add(ecosysPop);
+	}
+	ecosytems.Add(ecosysVal);
+
+	// color match
+	DBApi::findAllPlantsWithSomeTagsRequest colorRequest;
+	colorRequest.projName = FApp::GetProjectName();
+	TArray<DBApi::numberKVRequest> colorRanges;
+	DBApi::numberKV* colorH = numberTags.FindByPredicate([](DBApi::numberKV kv) { return kv.key == "Color_H"; });
+	DBApi::numberKV* colorS = numberTags.FindByPredicate([](DBApi::numberKV kv) { return kv.key == "Color_S"; });
+	DBApi::numberKV* colorV = numberTags.FindByPredicate([](DBApi::numberKV kv) { return kv.key == "Color_V"; });
+	if (!colorH || !colorS || !colorV) {
+		// color tag missing
+	}
+	DBApi::numberKVRequest colorRange;
+	float HOff = 2.0;
+	colorRange.key = colorH->key;
+	colorRange.lowerBound = colorH->value - 360.0 * HOff / (colorS->value + HOff);
+	colorRange.upperBound = colorH->value + 360.0 * HOff / (colorS->value + HOff);
+	colorRanges.Add(colorRange);
+	float SOff = 20.0;
+	colorRange.key = colorS->key;
+	colorRange.lowerBound = colorS->value - SOff;
+	colorRange.upperBound = colorS->value + SOff;
+	float VOff = 50.0;
+	colorRange.key = colorV->key;
+	colorRange.lowerBound = colorV->value - VOff;
+	colorRange.upperBound = colorV->value + VOff;
+	colorRequest.numberTags = colorRanges;
+	DBApi::findAllPlantsWithSomeTagsResult colorRes = DBApi::findAllPlantsWithSomeTags(colorRequest);
+	if (colorRes.errCode) {
+		// error handle
+	}
+
+	// ecosystem & layer filter
+	TArray<FString> candidates;
+	for (DBApi::plant plant : colorRes.plants) {
+		if (plant.hierarchy != (int)targetLayer) {
+			continue;
+		}
+
+		TArray<DBApi::stringKV> strTags;
+		TArray<DBApi::numberKV> numTags;
+		int errCode = GetPlantTags(plant.name, strTags, numTags);
+		if (errCode) {
+			// error handle
+		}
+
+		// phase ecosystem
+		bool ecoMatch = false;
+		DBApi::stringKV* ecoKV = strTags.FindByPredicate([](DBApi::stringKV kv) { return kv.key == "Ecosystem"; });
+		if (!ecoKV) {
+			// ecosystem tag missing
+		}
+		FString ecoPop, ecoVal = ecoKV->value;
+		while (ecoVal.Split("/", &ecoPop, &ecoVal)) {
+			if (ecosytems.Contains(ecoPop)) {
+				ecoMatch = true;
+				break;
+			}
+		}
+		ecoMatch |= ecosytems.Contains(ecoVal);
+		if (!ecoMatch) {
+			continue;
+		}
+
+		candidates.Add(plant.name);
+	}
 
 	// similarity match
+	/*
 	DBApi::findAllPlantsWithSomeTagsRequest similarRequest;
 	similarRequest.projName = FApp::GetProjectName();
 	similarRequest.stringTags = stringTags;
@@ -35,14 +109,9 @@ TArray<FString> UPlantRecommend::GetRecommendPlants(FString mainTreeName, Foliag
 	if (similarRes.errCode) {
 		// error handle
 	}
-	
-	TArray<FString> candidates;
-	for (DBApi::plant plant : similarRes.plants) {
-		if (plant.hierarchy != (int)targetLayer) {
-			continue;
-		}
-		candidates.Add(plant.name);
-	}
+	*/
+
+	// sort?
 
 	return candidates;
 }
